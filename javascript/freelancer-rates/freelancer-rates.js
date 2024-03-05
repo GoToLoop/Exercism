3144edef8ef3fcc58b6a8831e1873a49e4326578
GoToLoop <EMLF@HotMail.com>
2024-03-05 00:11:58 -0300

    poetry-club-door-policy

--- javascript/freelancer-rates/freelancer-rates.js
+++ javascript/freelancer-rates/freelancer-rates.js
@@ -1,25 +1,21 @@
-// @ts-check
-
-// Get those rates calculated!
-
-const WORK_HOURS = 8, BILLABLE_DAYS = 22;
+const DAILY_WORK_HOURS = 8, MONTHLY_BILLABLE_DAYS = 22;
 
 /**
- * Calculates the day rate given a rate per hour.
+ * Calculates the day rate pay given a rate per hour.
  *
- * @param {number} ratePerHour the rate per hour
- * @returns {number} the rate per day
+ * @param {number} ratePerHour the rate pay per hour
+ * @returns {number} the rate pay per day
  */
 export function dayRate(ratePerHour) {
-  return WORK_HOURS * ratePerHour;
+  return DAILY_WORK_HOURS * ratePerHour;
 }
 
 /**
  * Calculates the number of workdays given a fixed budget rounded down.
  *
  * @param {number} budget the total budget
- * @param {number} ratePerHour the rate per hour
- * @returns {number} the number of days rounded down
+ * @param {number} ratePerHour the rate pay per hour
+ * @returns {number} the number of working days rounded down
  */
 export function daysInBudget(budget, ratePerHour) {
   return budget / dayRate(ratePerHour) | 0;
@@ -29,16 +25,16 @@ export function daysInBudget(budget, ratePerHour) {
  * Calculates the discounted cost for large projects, rounding up final price.
  * Discount is applied to full months only. Remaining days are billed fully.
  *
- * @param {number} ratePerHour the rate per hour
+ * @param {number} ratePerHour the rate pay per hour
  * @param {number} numDays the number of days the project spans
  * @param {number} discount project's discount rate (20% is represented as 0.2)
- * @returns {number} the deducted final price rounded up
+ * @returns {number} the deducted total price rounded up
  */
 export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
   const
-    fullDays = numDays % BILLABLE_DAYS,
-    discountDays = numDays - fullDays,
-    billedDays = discountDays * (1 - discount) + fullDays;
+    remainingDays = numDays % MONTHLY_BILLABLE_DAYS,
+    discountDays  = numDays - remainingDays,
+    billableDays  = (1 - discount) * discountDays + remainingDays;
 
-  return Math.ceil(dayRate(ratePerHour) * billedDays);
+  return Math.ceil(dayRate(ratePerHour) * billableDays);
 }
