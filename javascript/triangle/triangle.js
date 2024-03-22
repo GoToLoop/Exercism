59948c6442797d518564fcbc77b072ba81c0c584
GoToLoop <EMLF@HotMail.com>
2024-03-22 11:02:33 -0300

    triangle

---
+++ javascript/triangle/triangle.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+export class Triangle {
+  /** @param {number} a @param {number} b @param {number} c */
+  constructor(a, b, c) {
+    this.sides = Float32Array.of(a, b, c).map(Math.abs).sort();
+    this.uniques = new Set(this.sides); }
+
+  get isEquilateral() { return this.uniques.size == 1 && !!this.sides[0]; }
+
+  get isIsosceles() {
+    return this.isValid() && this.uniques.size && this.uniques.size < 3; }
+
+  get isScalene() { return this.isValid() && this.uniques.size == 3; }
+
+  isValid() {
+    const [ a, b, c ] = this.sides;
+    return a * b * c != 0 && a + b >= c; } }
