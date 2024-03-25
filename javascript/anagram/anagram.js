6303c50d47ee007c28957f1421ddb8f7f350ba6b
GoToLoop <EMLF@HotMail.com>
2024-03-25 17:44:54 -0300

    anagram

---
+++ javascript/anagram/anagram.js
@@ -0,0 +1,20 @@
+// @ts-check
+
+/**
+ * Finds anagrams of a given `word` within an array of `words`.
+ * @param {string} word the target `word` to find anagrams for
+ * @param {string[]} words the array of anagram candidates for input `word`
+ * @returns {string[]} an array of anagrams found in the `words` array
+ */
+export function findAnagrams(word, words, sorted = sortWord(word)) {
+  return words.filter(w => sortWord(w) == sorted && noCase.compare(w, word)); }
+
+/**
+ * Sorts the characters of a given word in alphabetical order.
+ * @param {string} w the word to sort and convert to lowercase
+ * @returns {string} the sorted lowercase version of the input word
+ */
+export const sortWord = w => [ ...w.toLowerCase() ].sort().join('');
+
+/** This is a case-insensitive string comparison instance of Collator. */
+const noCase = Intl.Collator(void 0, { sensitivity: 'base' });
--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -4,7 +4,7 @@
 export function classify(n=1) {
   if (n<=0) throw Error('Classification is only possible for natural numbers.');
 
-  for (var lim = ~~Math.sqrt(n), sum = -n, div = 0, i = 1; i <= lim; ++i)
+  for (var lim = ~~Math.sqrt(n |= 0), sum = -n, div = 0, i = 1; i <= lim; ++i)
     if (n % i == 0) sum += i + (div = n / i) * +(div != i);
 
   return sum == n ? 'perfect' : sum > n && 'abundant' || 'deficient'; }
