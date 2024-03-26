16f30a3163f8434eb5146a585c4a88768a9168b7
GoToLoop <EMLF@HotMail.com>
2024-03-26 08:30:10 -0300

    matching-brackets

---
+++ javascript/matching-brackets/matching-brackets.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+const BRACKETS = '{}[]()', isEven = (n=0) => !(n & 1);
+
+export function isPaireds(str='', stack='', idx=-1) { // stack of open brackets
+  for (const ch of str)  if ( ~(idx = BRACKETS.indexOf(ch)) ) // found bracket
+    if (isEven(idx)) stack += BRACKETS[idx]; // even is open bracket; so add it
+    else if (stack.at(-1) == BRACKETS[idx - 1]) stack = stack.slice(0, -1);
+    else return false; // if close bracket type isn't last index, it's a fail
+  return !stack; } // all bracket pairs match if stack is empty at the end
