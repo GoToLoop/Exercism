17db5cc12d5420025a3165d386b08742e0799a9c
GoToLoop <EMLF@HotMail.com>
2024-03-29 09:49:05 -0300

    rotational-cipher

---
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+const ord = (ch='') => ch.charCodeAt(0), chr = String.fromCharCode,
+      isUpper = (ch='') => !(ord(ch) & 0x20), LEN = 26, A = ord('A'),
+      ABC = Object.freeze(Array.from({ length: LEN }, (_, i) => chr(i + A)));
+
+export function rotate(chars='', rot=0, cypher='') {
+  for (const ch of chars) {
+    const idx = ABC.indexOf(ch.toUpperCase()), char = ABC[(idx + rot) % LEN];
+    cypher += ~idx ? isUpper(ch) && char || char.toLowerCase() : ch; }
+  return cypher; }
--- javascript/transpose/transpose.js
+++ javascript/transpose/transpose.js
@@ -1,13 +1,45 @@
 // @ts-check
 
-/** @param {*[]} arr */
-const toLen = arr => arr.map(getLen), getLen = ({ length }) => length,
-      maxLen = arr => Math.max(...toLen(arr));
+/**
+ * Transposes an array of strings.
+ * @param {string[]} texts an array of strings to transpose
+ * @returns {string[]} the transposed array of strings
+ */
+export function transpose(texts) {
+  const lens = toLens(texts), len = maxLen(lens), idx = idxOfLen(lens, len);
 
-/** @param {string[]} texts */
-export function transpose(texts, transposed=[...''], len=maxLen(texts)) {
-  for (var txt = '', ch = '', l = texts.length, j = 0, i = 0; i < len; ++i) {
+  /** @type {string[]} transposed array = length of biggest text */
+  const transposed = Array(len > 0 && len || 0).fill(''), l = texts.length;
+
+  for (var txt = '', ch = '', j = 0, i = 0; i < len; ++i) {
     while (j < l) if ( ch = texts[j++][i] ) txt += ch;
-                  else if (i < maxLen( texts.slice(j) )) txt += ' ';
-    transposed.push(txt), txt = '', j = 0; }
+                  else if (i < idx || i < maxLen(lens.subarray(j))) txt += ' ';
+    transposed[i] = txt, txt = '', j = 0; }
+
   return transposed; }
+
+/**
+ * Represents an array-like container with numeric values.
+ * @typedef {ArrayLike<number> & Iterable<number>} NumArr
+ */
+
+/**
+ * Gets the `length` properties of each string from `arr`.
+ * @param {ArrayLike<string>} arr an array of strings
+ */
+const toLens = arr => Uint8Array.from(arr, getLen), getLen = arr => arr.length;
+
+/**
+ * Given an array of `length` values, finds which one is longest.
+ * @param {NumArr} lens an array of `length` values
+ * @returns {number} the biggest `length` value
+ */
+const maxLen = lens => Math.max(...lens);
+
+/**
+ * Given an array of `length` values, finds its index.
+ * @param {NumArr} lens an array of `length` values
+ * @param {number} len the `length` value to search for
+ * @returns {number} the index of the last occurrence of `len` in `lens`
+ */
+const idxOfLen = (lens, len) => Array.prototype.lastIndexOf.call(lens, len);
