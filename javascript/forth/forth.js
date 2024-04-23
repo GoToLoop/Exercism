29ef6e69e3b202fd64602e47f7d9a04bf48ff749
GoToLoop <EMLF@HotMail.com>
2024-04-23 02:30:03 -0300

    forth

---
+++ javascript/forth/forth.js
@@ -0,0 +1,48 @@
+// @ts-check
+
+/** @typedef {Object<string, string>} Cmd */
+
+var last = 0, penult = 0;
+
+export class Forth {
+  stack = /** @type {number[]} */([]); cmds = /** @type {Cmd} */({});
+
+  evaluate(exp='', {stack, cmds}=this) {
+    if ((exp = exp.toLowerCase())[0] == ':') return this.#define(exp);
+
+    for (const cmd of exp.split(' '))
+      if (isFinite(+cmd)) stack.push(+cmd | 0);
+      else if (cmds[cmd]) this.evaluate(cmds[cmd]);
+      else if (!this[cmd]?.()) throw Error('Unknown command'); return this; }
+
+  #define(exp='', {cmds}=this, idx=0) {
+    const [ cmd, ...args ] = exp.slice(2, -2).split(' ');
+    if (isFinite(+cmd)) throw Error('Invalid definition');
+    for (const arg of args) args[idx++] = cmds[arg] || args[idx-1];
+    return cmds[cmd] = args.join(' '), this; }
+
+  #empty(len=this.stack.length) { if (len < 2) throw Error('Stack empty'); }
+
+  #top({stack}=this, len=stack.length) {
+    if (len) return last = stack[len-1], this; throw Error('Stack empty'); }
+
+  #top2({stack}=this, len=stack.length) {
+    return this.#empty(), last = stack[len-1], penult = stack[len-2], this; }
+
+  #pop2({stack: s}=this) {
+    return this.#empty(), last = s.pop() || 0, penult = s.pop() || 0, this; }
+
+  over() { return this.#top2().stack.push(penult), this; }
+  dup() { return this.#top().stack.push(last), this; }
+  drop() { return this.#top().stack.pop(), this; }
+
+  swap({stack}=this, len=stack.length) {
+    return this.#top2().stack[len-1] = penult, stack[len-2] = last, this; }
+
+  '+'() { return this.#pop2().stack.push(penult + last), this; }
+  '-'() { return this.#pop2().stack.push(penult - last), this; }
+  '*'() { return this.#pop2().stack.push(penult * last), this; }
+
+  '/'() {
+    if (this.#pop2(), last) return this.stack.push(penult / last | 0), this;
+    throw Error('Division by zero'); } }
