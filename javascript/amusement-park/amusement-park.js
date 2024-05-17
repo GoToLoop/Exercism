559b424707914f6aba008acdb5b4e37ec8ac3145
GoToLoop <EMLF@HotMail.com>
2024-05-16 23:42:29 -0300

    better descriptions

--- javascript/amusement-park/amusement-park.js
+++ javascript/amusement-park/amusement-park.js
@@ -20,23 +20,25 @@ export const createVisitor = (name, age, ticketId) => ({ name, age, ticketId });
 export const revokeTicket = visitor => (visitor.ticketId = null, visitor);
 
 /**
- * Determines the status a ticket has in the ticket tracking object.
+ * Determines the status a ticket has in the ticket tracking object, either
+ * the owner of the `ticketId` if found or some error message.
  *
  * @param {Object<string, string?>} tickets object tracking all tickets
  * @param {string} ticketId the ID of the ticket to check
- * @returns {string} ticket status
+ * @returns {string} either the corresponding owner of the `ticketId` or
+ * its id status: 'not sold' when `null` or 'unknown' otherwise
  */
-export function ticketStatus(tickets, ticketId, _status=tickets[ticketId]) {
-  if (_status) return 'sold to ' + _status;
-  return _status === null ? 'not sold' : 'unknown ticket id'; }
+export function ticketStatus(tickets, ticketId, _owner=tickets[ticketId]) {
+  if (_owner) return 'sold to ' + _owner;
+  return _owner === null ? 'not sold' : 'unknown ticket id'; }
 
 /**
- * Determines the status a ticket has in the ticket tracking object
- * and returns a simplified status message.
+ * Determines the status a ticket has in the ticket tracking object and
+ * returns a simplified status message.
  *
  * @param {Object<string, string?>} tickets object tracking all tickets
  * @param {string} ticketId the ID of the ticket to check
- * @returns {string} ticket status
+ * @returns {string} ticket status or its owner
  */
 export const simpleTicketStatus = (tickets, ticketId) =>
   tickets[ticketId] ?? 'invalid ticket !!!';
