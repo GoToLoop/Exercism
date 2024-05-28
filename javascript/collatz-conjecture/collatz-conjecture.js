7fa0f5250667561ca8acafbc396eb7c2adf957ce
GoToLoop <EMLF@HotMail.com>
2024-05-28 02:17:51 -0300

    collatz-conjecture

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,5 +3,4 @@
 /** @param {number} n a positive integer */
 export function steps(n, count=0) {
   if ((n |= 0) < 1) throw Error('Only positive numbers are allowed');
-  while (n != 1) n = n & 1 && 3*n + 1 || n >> 1, ++count;
-  return count; }
+  while (n != 1) n = n & 1 && 3*n + 1 || n >> 1, ++count; return count; }
