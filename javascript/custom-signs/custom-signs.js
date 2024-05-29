8369107b956b4cf0ffe94e390b15f946e63bdafe
GoToLoop <EMLF@HotMail.com>
2024-05-29 06:08:12 -0300

    custom-signs

--- javascript/custom-signs/custom-signs.js
+++ javascript/custom-signs/custom-signs.js
@@ -1,40 +1,51 @@
 // @ts-check
 
 /**
- * Build a sign that includes both of the parameters.
+ * Builds a sign for a specific `occasion` including the given `name`.
  *
- * @param {string} occasion
- * @param {string} name
- * @returns {string} template string combining both parameters
+ * @param {string} occasion The occasion for the sign
+ * (e.g., “Birthday,” “Anniversary”).
+ * @param {string} name The name to include in the sign.
+ *
+ * @returns {`Happy ${string} ${string}!`} A template string combining the
+ * `occasion` and the `name`.
  */
 export const buildSign = (occasion, name) => `Happy ${occasion} ${name}!`;
 
 /**
- * Build a birthday sign that conditionally formats the return string.
+ * Builds a birthday sign with conditional formatting based on the person’s age.
  *
- * @param {number} age
- * @returns {string} template string based on age
+ * @param {number} age The person’s age.
+ *
+ * @returns {`Happy Birthday! What a ${'mature' | 'young'} fellow you are.`}
+ * A template string with a birthday message based on the `age`.
  */
 export const buildBirthdaySign = age =>
   `Happy Birthday! What a ${age >= 50 && 'mature' || 'young'} fellow you are.`;
 
 /**
- * Build a graduation sign that includes multiple lines.
+ * Builds a multiline graduation sign for a specific person and graduation year.
  *
- * @param {string} name
- * @param {number} year
- * @returns {string} multi-line template string
+ * @param {string} name The graduate’s name.
+ * @param {number} year The graduation year.
+ *
+ * @returns {`Congratulations ${string}!\nClass of ${number}`}
+ * A multiline template string congratulating the graduate.
  */
 export const graduationFor = (name, year) => `Congratulations ${name}!
 Class of ${year}`;
 
 /**
- * Determine cost based on each character of sign parameter that builds
- * the template string that includes the currency parameter.
+ * Determines the cost of creating a sign based on the number of characters in
+ * the sign.
  *
- * @param {string} sign
- * @param {string} currency
- * @returns {string} cost to create the sign
+ * @param {string} sign The sign content.
+ * @param {string} currency The plural of the currency type
+ * (e.g., “dollars”, “euros”).
+ *
+ * @returns {`Your sign costs ${number}.00 ${string}.`}
+ * A template string informing the calculated cost to create the `sign` in the
+ * specified `currency` based on the number of characters of that `sign`.
  */
 export const costOf = (sign, currency) =>
-  `Your sign costs ${(sign.length * 2 + 20).toFixed(2)} ${currency}.`;
+  `Your sign costs ${sign.length * 2 + 20}.00 ${currency}.`;
