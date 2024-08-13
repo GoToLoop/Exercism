5314e9a5984ba4152be95ab52c906a1dae5627f4
GoToLoop <EMLF@HotMail.com>
2024-08-13 14:41:25 -0300

    minesweeper

---
+++ python/minesweeper/minesweeper.py
@@ -0,0 +1,14 @@
+def annotate(board: list[str], chars=''):
+    if any(len(row) != len(board[0]) or row.strip(' *') for row in board):
+        raise ValueError('The board is invalid with current input.')
+
+    for r, row in enumerate(b := board.copy()):
+        for c, mine_or_empty in enumerate(row):
+            if mine_or_empty == '*': chars += mine_or_empty; continue
+
+            n = sum(r[max(c-1, 0):c+2].count('*') for r in b[max(r-1, 0):r+2])
+            chars += n and str(n) or mine_or_empty
+
+        b[r] = chars; chars = ''
+
+    return print(board, b, sep='\n') or b
