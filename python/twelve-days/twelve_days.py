d1db39f86320e4bba1041fc2491e2bcacc823ceb
GoToLoop <EMLF@HotMail.com>
2024-09-03 12:43:54 -0300

    versify()

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,34 +1,55 @@
-def recite(lo: int, hi: int) -> list[str]:
-    """Recite the verses of the song "12 Days of Christmas" for a given range.
+"""a Partridge in a Pear Tree.
+two Turtle Doves, 
+three French Hens, 
+four Calling Birds, 
+five Gold Rings, 
+six Geese-a-Laying, 
+seven Swans-a-Swimming, 
+eight Maids-a-Milking, 
+nine Ladies Dancing, 
+ten Lords-a-Leaping, 
+eleven Pipers Piping, 
+twelve Drummers Drumming, \rfirst
+second
+third
+fourth
+fifth
+sixth
+seventh
+eighth
+ninth
+tenth
+eleventh
+twelfth"""
 
-    This function generates the lyrics for the song based on the specified
-    range of days. Each verse corresponds to a day, and the verses are recited
-    in reverse order for each day.
+DOC = __doc__ or ''; G, D = ((*d.split('\n'),) for d in DOC.split('\r'))
+I = 'On the %s day of Christmas my true love gave to me: '; A = 'and '
 
-    :param int lo: Starting day (1 to 12) of the song verse.
-    :param int hi: Ending day (1 to 12) of the song verse.
+recite = lambda lo=1, hi=12: [ *map(versify, range(lo - 1, hi)) ]
+"""Recite the verses of the song "12 Days of Christmas" for a given range.
 
-    :return list[str]: A list containing the string of the joined verses for
-    the given range of days when `lo` and `hi` are the same. Otherwise, it
-    returns a list of strings, each representing the joined verses for each
-    day in the specified range.
-    """
-    # if lo * hi == 0 or lo | hi < 0: return ['']
+This function generates the lyrics for the song based on the specified
+range of days. Each verse corresponds to the gifts received on a day, and they
+are recited in reverse order for each day. If `lo` and `hi` are the same, it
+just returns a single verse.
 
-    if lo != hi: return [ recite(i, i)[0] for i in range(lo, hi + 1) ]
+:param int lo: Starting day (1 to 12) of the song verse (inclusive).
+:param int hi: Ending day (1 to 12) of the song verse (inclusive).
 
-    verses = [ INTRO % DAYS[lo - 1], *GIFTS[lo - 1::-1] ]
+:return list[str]: A list containing the verse for the specified day if both
+`lo` and `hi` are the same. Otherwise, it returns a list of strings, each
+representing the verse for each day in the specified range.
+"""
 
-    if lo != 1: verses[-1] = AND + verses[-1]
+versify = lambda i=0: I % D[i] + ''.join(G[i:0:-1]) + (i and A or '') + G[0]
+"""Generate the verse for the specified day of Christmas.
 
-    return print(verses := [ ''.join(verses) ]) or verses
+This function constructs the verse for the given day by combining the
+appropriate lines of the song in reverse order, starting from the gifts
+received on that day down to the gifts from previous days until the first day.
 
+:param int i: The day of Christmas for which to generate the verse\
+(0 to 11, where 0 is the first and 11 is the twelfth day).
 
-INTRO = 'On the %s day of Christmas my true love gave to me: '; AND = 'and '
-
-GIFTS = ('a Partridge in a Pear Tree.', 'two Turtle Doves, ',
-'three French Hens, ', 'four Calling Birds, ', 'five Gold Rings, ',
-'six Geese-a-Laying, ', 'seven Swans-a-Swimming, ', 'eight Maids-a-Milking, ',
-'nine Ladies Dancing, ', 'ten Lords-a-Leaping, ', 'eleven Pipers Piping, ',
-'twelve Drummers Drumming, '); DAYS = ('first', 'second', 'third', 'fourth',
-'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth')
+:return str: The verse for the specified day of the song.
+"""
