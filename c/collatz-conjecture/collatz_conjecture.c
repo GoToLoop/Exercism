7de0dbde67992ca4ad4a6c2a8e23ced2e52d7db5
GoToLoop <EMLF@HotMail.com>
2024-10-28 09:41:39 -0300

    Just "collatz_conjecture.c"

--- c/collatz-conjecture/collatz_conjecture.c
+++ c/collatz-conjecture/collatz_conjecture.c
@@ -1,1 +1,5 @@
 #include "collatz_conjecture.h"
+
+inline int steps(int n) {
+  int c = 0; if (n < 1) return ERROR_VALUE;
+  while (n != 1) { n = n & 1? 3*n + 1 : n >> 1, ++c; } return c; }
--- c/collatz-conjecture/collatz_conjecture.h
+++ c/collatz-conjecture/collatz_conjecture.h
@@ -1,3 +1,1 @@
-static const char ERROR_VALUE = -1; inline static int steps(int n) {
-  int c = 0; if (n < 1) return ERROR_VALUE;
-  while (n != 1) { n = n & 1? 3*n + 1 : n >> 1, ++c; } return c; }
+static const char ERROR_VALUE = -1; int steps(int n);
