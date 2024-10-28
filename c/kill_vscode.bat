93a063b51633dd7dccc87f0dfe64706f8e5db7b4
GoToLoop <EMLF@HotMail.com>
2024-10-28 09:56:59 -0300

    Eliminate all .vscode subfolders!

---
+++ c/kill_vscode.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+SETLOCAL
+
+SET VSCODE=.vscode
+SET /A COUNT=0
+
+FOR /D %%d IN (*) DO (
+    iF EXIST %%d\%VSCODE% (
+        ECHO Deleting %VSCODE% in %%d...
+        RMDIR /S /Q %%d\%VSCODE%
+        SET /A COUNT+=1
+    )
+)
+
+ECHO All %VSCODE% folders have been deleted.
+ECHO Total folders deleted: %COUNT%
+
+ENDLOCAL
