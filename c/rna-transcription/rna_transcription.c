3fcbbcbcc1e56c278a6f23b8cde5f4ddf23d8d52
GoToLoop <EMLF@HotMail.com>
2024-11-03 22:02:23 -0300

    #define hack(ch) "UCG A"[ch % 5]

--- c/rna-transcription/rna_transcription.c
+++ c/rna-transcription/rna_transcription.c
@@ -4,12 +4,15 @@
 #include <string.h>
 
 #define alloc(str) calloc(strlen(str) + 1, 1)
+#define hack(ch) "UCG A"[ch % 5]
 
 /* inline static char *clone(const char *src) { // same as strdup()
-  char *c = malloc(strlen(src) + 1); if (c) strcpy(c, src); return c; } */
+  char *c = malloc(strlen(src) + 1); if (c) strcpy(c, src); return c; }
+ */
 
-static char shift(const char c) {
+/* static char shift(const char c) {
   int i = -1; while (++i < 4) if ("GCTA"[i] == c) return "CGAU"[i]; return 0; }
+ */
 
 char *to_rna(const char *dna) {
-  char *r = alloc(dna), *t = r; while (*dna) *t++ = shift(*dna++); return r; }
+  char *r = alloc(dna), *t = r; while (*dna) *t++ = hack(*dna++); return r; }
