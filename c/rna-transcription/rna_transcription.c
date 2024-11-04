7c5b20103b2df1e81e9c3fe94873b7a451010e3b
GoToLoop <EMLF@HotMail.com>
2024-11-03 21:25:28 -0300

    #define alloc(str) calloc(strlen(str) + 1, 1)

--- c/rna-transcription/rna_transcription.c
+++ c/rna-transcription/rna_transcription.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-static const char *const DNA = "GCTA", *const RNA = "CGAU";
+#define alloc(str) calloc(strlen(str) + 1, 1)
 
-inline static char shift(char a) {
-  int i = -1; while (++i < 4) if (DNA[i] == a) return RNA[i]; return 0; }
+/* inline static char *clone(const char *src) { // same as strdup()
+  char *c = malloc(strlen(src) + 1); if (c) strcpy(c, src); return c; } */
 
-inline static char *clone(const char *s) { // strdup()
-  long l = 1+strlen(s); char *c = malloc(l); if (c) memcpy(c, s, l); return c; }
+static char shift(const char c) {
+  int i = -1; while (++i < 4) if ("GCTA"[i] == c) return "CGAU"[i]; return 0; }
 
 char *to_rna(const char *dna) {
-  char *r = clone(dna), *t = r; while (*dna) *t++ = shift(*dna++); return r; }
+  char *r = alloc(dna), *t = r; while (*dna) *t++ = shift(*dna++); return r; }
