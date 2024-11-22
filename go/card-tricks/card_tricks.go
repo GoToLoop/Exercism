47634263aaf28d3136259359ca35805b447849ae
GoToLoop <EMLF@HotMail.com>
2024-11-21 21:57:00 -0300

    card_tricks.go

---
+++ go/card-tricks/card_tricks.go
@@ -0,0 +1,31 @@
+package cards
+
+// FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
+func FavoriteCards() []int { return []int { 2, 6, 9 } }
+
+// GetItem retrieves an item from a slice at given position.
+// If the index is out of range, we want it to return -1.
+func GetItem(l []int, i int) int { if inRange(l, i) { return l[i] }; return -1 }
+
+// SetItem writes an item to a slice at given position overwriting an
+// existing value. If the index is out of range the value needs to be appended.
+func SetItem(lst []int, i, v int) []int {
+	if inRange(lst, i) { lst[i] = v } else { lst = append(lst, v) }; return lst }
+
+// PrependItems adds an arbitrary number of values at the front of a slice.
+func PrependItems(lst []int, vals ...int) []int { return append(vals, lst...) }
+
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(lst []int, i int) []int {
+	if inRange(lst, i) { lst = append(lst[:i], lst[i+1:]...) }; return lst }
+
+// inRange checks if the given index is within the bounds of the provided slice.
+// It returns true if the index is valid, otherwise it returns false.
+//
+// Parameters:
+// - lst: A slice of any type.
+// - idx: The index to check.
+//
+// Returns:
+// - bool: true if the index is within bounds, false otherwise.
+func inRange[T any](lst []T, i int) bool { return i >= 0 && i < len(lst) }
