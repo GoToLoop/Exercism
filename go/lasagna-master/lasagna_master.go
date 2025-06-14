98a467bb9289d6912f797e62db8f0ffa5ea647fa
GoToLoop <EMLF@HotMail.com>
2025-06-14 01:02:34 -0300

    Lasagna make()

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,7 +1,7 @@
 package lasagna
 
 func PreparationTime(layers []string, time int) int {
-	if time <= 0 { time = 2 }; return len(layers) * time }
+	if time <= 0 { time = 2 }; return time * len(layers) }
 
 func Quantities(layers []string) (grams int, liters float64) {
 	for _, layer := range layers { switch layer {
@@ -9,6 +9,6 @@ func Quantities(layers []string) (grams int, liters float64) {
 
 func AddSecretIngredient(his, my []string) { my[len(my)-1] = his[len(his)-1] }
 
-func ScaleRecipe(amounts []float64, portions int) (scaled []float64) {
-	scale := .5 * float64(portions); scaled = append(scaled, amounts...)
-	for idx := range scaled { scaled[idx] *= scale }; return }
+func ScaleRecipe(amounts []float64, portions int) []float64 {
+	scaled, scale := make([]float64, len(amounts)), .5 * float64(portions)
+	for idx, qty := range amounts { scaled[idx] = qty * scale }; return scaled }
