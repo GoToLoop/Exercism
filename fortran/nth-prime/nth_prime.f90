b0155bf2fb293dc414e77a2e6fa6e20a0416f6e5
GoToLoop <EMLF@HotMail.com>
2025-12-20 17:11:59 -0300

    nth_prime.f90 (optimized)

--- fortran/nth-prime/nth_prime.f90
+++ fortran/nth-prime/nth_prime.f90
@@ -1,8 +1,8 @@
 MODULE nth_prime; CONTAINS
 
-  PURE INTEGER FUNCTION prime(n); INTEGER, INTENT(IN) :: n; prime = -1
-    IF (n < 1) RETURN; i = 0; prime = 1; DO; prime = prime + 1
-      IF (isPrime(prime)) THEN; i = i + 1; IF (i == n) EXIT; END IF; END DO; END
+  PURE INTEGER FUNCTION prime(n) RESULT(p); INTEGER, INTENT(IN) :: n
+    p = merge(2, -1, n == 1); IF (n < 2) RETURN; i = 1; p = 1; DO; p = p + 2
+      IF (isPrime(p)) THEN; i = i + 1; IF (i == n) EXIT; END IF; END DO; END
 
   PURE LOGICAL FUNCTION isPrime(n); INTEGER, INTENT(IN) :: n; isPrime = .TRUE.
     IF (n <= 3) THEN; isPrime = n >= 2; RETURN; END IF
