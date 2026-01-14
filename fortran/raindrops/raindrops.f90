4faf8fde9f0f85a99d1932c523cabb5638e66732
GoToLoop <EMLF@HotMail.com>
2026-01-14 17:46:29 -0300

    Lang icons

--- fortran/raindrops/raindrops.f90
+++ fortran/raindrops/raindrops.f90
@@ -1,11 +1,10 @@
 MODULE raindrops; CONTAINS
-
   PURE CHARACTER(15) FUNCTION convert(n) RESULT(s); INTENT(IN) n
 
-    INTEGER, PARAMETER :: PRIMES(3) = (/3, 5, 7/)
+    INTEGER(1), PARAMETER :: PRIMES(3) = (/3_1, 5_1, 7_1/)
     CHARACTER(5), PARAMETER :: NOISES(3) = (/"Pling", "Plang", "Plong"/)
 
     s = ""; DO i = 1, size(PRIMES)
-      IF (mod(n, PRIMES(i)) == 0) s(1 + len_trim(s):) = NOISES(i); END DO
+      IF (mod(n, int(PRIMES(i))) == 0) s(1 + len_trim(s):) = NOISES(i); END DO
 
     IF (s .EQ. '') WRITE (s, '(I0)') n; END FUNCTION; END MODULE
