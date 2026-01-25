842b2ea1555311bedd683547fd9f35994aec6f58
GoToLoop <EMLF@HotMail.com>
2026-01-25 06:43:28 -0300

    Use pack(comprehension) for uni()

--- fortran/triangle/triangle.f90
+++ fortran/triangle/triangle.f90
@@ -1,37 +1,33 @@
-MODULE triangle; INTERFACE scalene; MODULE PROCEDURE scal, scal_i; END INTERFACE
-  INTERFACE equilateral; MODULE PROCEDURE equi, equi_i; END INTERFACE
-  INTERFACE isosceles; MODULE PROCEDURE isos, isos_i; END INTERFACE; CONTAINS
+MODULE triangle; INTERFACE scalene; MODULE PROCEDURE scal, i_scal; END INTERFACE
+  INTERFACE equilateral; MODULE PROCEDURE equi, i_equi; END INTERFACE
+  INTERFACE isosceles; MODULE PROCEDURE isos, i_isos; END INTERFACE; CONTAINS
 
   PURE LOGICAL FUNCTION valid(t); REAL, INTENT(IN) :: t(3) ! is triangle
     valid = all(t > 0) .AND. 2*maxval(t) < sum(t); END FUNCTION
 
-  PURE LOGICAL FUNCTION valid_i(t); INTEGER, INTENT(IN) :: t(3) ! is triangle
-    valid_i = all(t > 0) .AND. 2*maxval(t) < sum(t); END FUNCTION
+  PURE LOGICAL FUNCTION i_valid(t); INTEGER, INTENT(IN) :: t(3) ! is triangle
+    i_valid = all(t > 0) .AND. 2*maxval(t) < sum(t); END FUNCTION
 
-  PURE FUNCTION unique(arr) RESULT(uni); REAL, INTENT(IN) :: arr(:)
-    REAL, ALLOCATABLE :: uni(:); uni = [arr(1)]; DO i = 2, size(arr)
+  PURE FUNCTION uni(arr); REAL, INTENT(IN) :: arr(:); ALLOCATABLE uni(:)
+    uni = pack(arr, [(.NOT. any(arr(i) == arr(:i - 1)), i=1, size(arr))]); END
 
-      a = arr(i); IF (.NOT. any(uni == a)) uni = [uni, a]; END DO; END FUNCTION
-
-  PURE FUNCTION unique_i(arr) RESULT(uni); INTEGER, INTENT(IN) :: arr(:)
-    INTEGER, ALLOCATABLE :: uni(:); uni = [arr(1)]; DO i = 2, size(arr)
-
-      n = arr(i); IF (.NOT. any(uni == n)) uni = [uni, n]; END DO; END FUNCTION
+  PURE FUNCTION i_uni(arr); INTEGER, INTENT(IN) :: arr(:); ALLOCATABLE i_uni(:)
+    i_uni = pack(arr, [(.NOT. any(arr(i) == arr(:i - 1)), i=1, size(arr))]); END
 
   PURE LOGICAL FUNCTION scal(t); REAL, INTENT(IN) :: t(3) ! scalene
-    scal = valid(t) .AND. size(unique(t)) == 3; END FUNCTION
+    scal = valid(t) .AND. size(uni(t)) == 3; END FUNCTION
 
-  PURE LOGICAL FUNCTION scal_i(t); INTEGER, INTENT(IN) :: t(3) ! scalene
-    scal_i = valid_i(t) .AND. size(unique_i(t)) == 3; END FUNCTION
+  PURE LOGICAL FUNCTION i_scal(t); INTEGER, INTENT(IN) :: t(3) ! scalene
+    i_scal = i_valid(t) .AND. size(i_uni(t)) == 3; END FUNCTION
 
   PURE LOGICAL FUNCTION equi(t); REAL, INTENT(IN) :: t(3) ! equilateral
-    equi = valid(t) .AND. size(unique(t)) == 1; END FUNCTION
+    equi = valid(t) .AND. size(uni(t)) == 1; END FUNCTION
 
-  PURE LOGICAL FUNCTION equi_i(t); INTEGER, INTENT(IN) :: t(3) ! equilateral
-    equi_i = valid_i(t) .AND. size(unique_i(t)) == 1; END FUNCTION
+  PURE LOGICAL FUNCTION i_equi(t); INTEGER, INTENT(IN) :: t(3) ! equilateral
+    i_equi = i_valid(t) .AND. size(i_uni(t)) == 1; END FUNCTION
 
   PURE LOGICAL FUNCTION isos(t); REAL, INTENT(IN) :: t(3) ! isosceles
-    l = size(unique(t)); isos = valid(t) .AND. l > 0 .AND. l < 3; END FUNCTION
+    l = size(uni(t)); isos = valid(t) .AND. l > 0 .AND. l < 3; END FUNCTION
 
-  PURE LOGICAL FUNCTION isos_i(t); INTEGER, INTENT(IN) :: t(3) ! isosceles
-    l = size(unique_i(t)); isos_i = valid_i(t) .AND. l > 0 .AND. l < 3; END; END
+  PURE LOGICAL FUNCTION i_isos(t); INTEGER, INTENT(IN) :: t(3) ! isosceles
+    l = size(i_uni(t)); i_isos = i_valid(t) .AND. l > 0 .AND. l < 3; END; END
